a496780ae7f3e8b1dcd98fd0da477d00fe81776c
Markus-Stefan Anni <markus-stefan.anni@voco.ee>
2022-11-02 02:20:45 -0700

    Ülesanne: Funktsioon

---
+++ Function.py
@@ -0,0 +1,34 @@
+#1. Inside the function
+
+def func():
+    print("I´m inside the function")
+
+#func()
+
+#2. My name is ...
+def my_name_is(name):
+    print(f"My name is {name}")
+
+#my_name_is("Mari")
+#my_name_is("Stefan")
+
+#3. Sum six
+def sum_six(num):
+    return (6 + num)
+#print(sum_six(5))
+
+#4. Sum numbers
+def sum_numbers(a, b):
+    return (a + b)
+print(sum_numbers(15, 25))
+
+
+#5. USD to EUR
+def usd_to_eur(dollar):
+    return round(dollar * 0.8)
+print(usd_to_eur(100))
+
+
+
+
+
